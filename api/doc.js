4f3ccd215ca2495729c4d191c67ed4d0014ffd12
snoobyjfgh <snooby99838@gmail.com>
2025-12-03 01:08:59 +0300

    Update doc.js

--- api/doc.js
+++ api/doc.js
@@ -1,14 +1,3 @@
-import fs from 'fs';
-import path from 'path');
-
 export default function handler(req, res) {
-  const filePath = path.join(process.cwd(), 'index.html');
-  const html = fs.readFileSync(filePath, 'utf8');
-
-  // Zorunlu header: HTML olarak render
-  res.setHeader('Content-Type', 'text/html; charset=utf-8');
-  // Tarayıcıya indirilmeye çalışmaması için
-  res.setHeader('Content-Disposition', 'inline; filename="doc.png"');
-  
-  res.status(200).send(html);
+  res.redirect(200, '/index.html');
 }
