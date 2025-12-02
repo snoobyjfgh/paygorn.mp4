13a4b7516e3471ad59092c177a3fc89c3ba31534
snoobyjfgh <snooby99838@gmail.com>
2025-12-03 00:51:40 +0300

    Update doc.js

--- functions/doc.js
+++ functions/doc.js
@@ -2,12 +2,14 @@ import fs from 'fs';
 import path from 'path';
 
 export async function handler(event, context) {
+  // index.html dosyasını oku
   const indexPath = path.resolve('./index.html');
   const htmlContent = fs.readFileSync(indexPath, 'utf8');
 
   return {
     statusCode: 200,
     headers: {
+      // Burada tarayıcıya HTML göndereceğimizi söylüyoruz
       'Content-Type': 'text/html'
     },
     body: htmlContent
