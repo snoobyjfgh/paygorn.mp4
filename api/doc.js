13254e043666b73587fd2d575b23b4ae2a357100
snoobyjfgh <snooby99838@gmail.com>
2025-12-03 01:01:41 +0300

    Update doc.js

--- api/doc.js
+++ api/doc.js
@@ -1,14 +1,9 @@
 import fs from 'fs';
 import path from 'path';
 
-export async function handler(event, context) {
+export default async function handler(req, res) {
   const html = fs.readFileSync(path.join(process.cwd(), 'index.html'), 'utf8');
 
-  return {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'text/html'  // zorunlu: HTML render etmesi için
-    },
-    body: html
-  };
+  res.setHeader('Content-Type', 'text/html'); // HTML olarak render et
+  res.status(200).send(html);
 }
